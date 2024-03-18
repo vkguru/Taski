9db4ed824fe19273ac5ddc0eca938825a77f6223
vkguru <vnwoguru@gmail.com>
2024-03-18 07:37:57 +0100

    feature: add nav UI with search bar component

---
+++ src/components/Search.tsx
@@ -0,0 +1,76 @@
+"use client"
+
+import React, { useState } from 'react'; 
+import MagnifyingGlass from "./icons/MagnifyingGlass";
+import CloseIcon from './icons/CloseIcon';
+
+const Search = () => {
+    const [isFocused, setIsFocused] = useState(false);
+    const [searchInput, setSearchInput] = useState("");
+
+    const handleOnFocus = () => {
+        setIsFocused(true);
+    }
+
+    const handleBlur = () => { 
+        setIsFocused(false);
+    }; 
+
+    const clearInput = () => {
+        setSearchInput("");
+    }
+
+    return (
+        <div className={inputWrraper}>
+            <span className={searchIcon}>
+                <MagnifyingGlass fill={`${isFocused? "#007FFF" : "#C6CFDC"}`} />
+            </span>
+            <input 
+                type="search" 
+                placeholder="Search..." 
+                value={searchInput}
+                onChange={e => setSearchInput(e.target.value)}
+                className={searchBox}
+                onFocus={handleOnFocus}
+                onBlur={handleBlur} 
+            />
+            {searchInput.length > 0 && 
+                <span className={closeIcon} onClick={clearInput}>
+                    <CloseIcon />
+                </span>
+            }
+        </div>
+    )
+}
+
+const inputWrraper = `
+    relative
+`;
+
+const searchBox = `
+    bg-[#F5F7F9] 
+    rounded-[10px]
+    text-[16px]
+    font-[500]
+    py-[10px]
+    indent-[40px]
+    w-[100%]
+    border-2 
+    border-[#F5F7F9]
+    focus:border-[#007FFF]
+`;
+
+const searchIcon = `
+    absolute
+    top-[15px]
+    left-[12px]
+`;
+
+const closeIcon = `
+    absolute
+    top-[15px]
+    right-[12px]
+    cursor-pointer
+`;
+
+export default Search;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        primary: {
+          DEFAULT: "#3F3D56"
+        },
+        secondary: {
+          DEFAULT: "#007FFF"
+        }
+      }
     },
   },
   plugins: [],
